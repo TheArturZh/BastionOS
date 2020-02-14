0fdcad4f45b36b13f097201622587f42ff4a9cd1
Artur Zhidkov <thearturzh@gmail.com>
2020-02-15 01:44:07 +0300

    My shitcode now runs on bochs again, yey
    Not entirely sure why it wasn't working, this spooky shit requires some
    investigation.

--- src/kernel/gdt/gdt.h
+++ src/kernel/gdt/gdt.h
@@ -3,9 +3,9 @@
 
 namespace gdt {
 	namespace segments {
-		unsigned short code32 = 0x08;
-		unsigned short code64 = 0x10;
-		unsigned short data   = 0x18;
+		const unsigned short code32 = 0x08;
+		const unsigned short code64 = 0x10;
+		const unsigned short data   = 0x18;
 	}
 }
 
